80256981f5ede6b98efd9f99b5c570003e71862c
Ethan Randall <ethan@rabbit5.local>
2016-06-18 15:37:30 -0400

    cleaned up New Terminal at File

--- newtermatfile.py
+++ newtermatfile.py
@@ -9,7 +9,9 @@ tell application "Terminal"
     activate
     set var1 to "{0}"
     set var2 to "{1}"
-    do script ("cd " & quoted form of var1 & ";" & "tput clear; FINDER_FILE=" & quoted form of var2)
+    set currentTab to do script ("cd " & quoted form of var1 & ";")
+    do script ("tput clear;") in currentTab
+    do script ("FINDER_FILE=" & quoted form of var2 & ";") in currentTab
 end tell
 """
 
@@ -21,4 +23,4 @@ fp_out = fp_dn
 
 p1 = subprocess.Popen(['/usr/bin/osascript', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=False)
 
-stdo, stderr = p1.communicate(myscript.format(fp_out + "/", "./" + fp_bn))
+stdo, stderr = p1.communicate(myscript.format(fp_out + "/", "./" + str(fp_bn).splitlines()[0]))
